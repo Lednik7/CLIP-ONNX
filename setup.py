684d3eb425f8c66220ed72b589d1cab1d6166319
Lednik7 <gmaksim737@gmail.com>
2022-01-05 17:31:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ install_requires += ["clip @ git+https://github.com/openai/CLIP.git"]
 setup(
     name="clip_onnx",
     version="1.0",
+    py_modules=["clip_onnx, clip"],
     description="",
     author="Maxim Gerasimov",
     packages=find_packages(),
