fd61389a7c5ff31500a50d248d981c2b3f04477f
Gerasimov Maxim <52196169+Lednik7@users.noreply.github.com>
2022-05-03 09:28:32 +0300

    change opset_version to 12

--- clip_onnx/utils.py
+++ clip_onnx/utils.py
@@ -49,6 +49,6 @@ def attention(self, x: torch.Tensor):
 
 
 DEFAULT_EXPORT = dict(input_names=['input'], output_names=['output'],
-                      export_params=True, verbose=False, opset_version=14,
+                      export_params=True, verbose=False, opset_version=12,
                       do_constant_folding=True,
                       dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}})
